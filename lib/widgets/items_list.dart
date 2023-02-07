551075adc99e1229a8b345682fc9c8fb53e36f44
Danish <danishmaulahasbi@gmail.com>
2023-02-07 14:01:16 +0700

    initial commit

---
+++ lib/widgets/items_list.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import '../screens/detail_screen.dart';
+
+class ItemList extends StatelessWidget {
+  final String title;
+  final String description;
+  final String imageUrl;
+  const ItemList(
+      {super.key,
+      required this.title,
+      required this.description,
+      required this.imageUrl});
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: () {
+        Navigator.of(context)
+            .pushNamed(DetailScreen.routeName, arguments: title);
+      },
+      child: ListTile(
+        title: Text(title),
+        subtitle: Text(description),
+        leading: CircleAvatar(
+          radius: 30,
+          backgroundImage: NetworkImage(imageUrl),
+        ),
+      ),
+    );
+  }
+}
